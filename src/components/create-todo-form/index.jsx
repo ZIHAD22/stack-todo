6c8aaabfeae8c151ae114d3cb724269c69859668
ZIHAD22 <mdzihad2244@gmail.com>
2022-01-22 23:24:20 +0600

    create todo form

---
+++ src/components/create-todo-form/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap"; //react bootstrap
+
+// main component
+class CreateTodoForm extends Component {
+  state = {
+    text: "",
+    description: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.prevebtDefault();
+    this.props.createTodo(this.state);
+    event.target.reset();
+    this.setState({
+      text: "",
+      description: "",
+    });
+  };
+
+  render() {
+    return (
+      <Form>
+        <FormGroup onSubmit={this.handleSubmit}>
+          <Label>Enter Task</Label>
+          <Input
+            placeholder="Do some Code"
+            name="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Describe Task</Label>
+          <Input
+            placeholder="Write some short description about your task"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <Button type="submit">Create Task</Button>
+      </Form>
+    );
+  }
+}
+
+// proptypes of createTodoForm component
+CreateTodoForm.propTypes = {
+  createTodo: PropTypes.func.isRequired,
+};
+
+export default CreateTodoForm;
