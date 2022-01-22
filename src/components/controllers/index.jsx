f2b060cd6cf6d4de9ba207241a35141a6b330e7c
ZIHAD22 <mdzihad2244@gmail.com>
2022-01-22 23:46:30 +0600

    Controller component is completed

---
+++ src/components/controllers/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+// our Custom Components
+import SearchPanel from "./search-panel";
+
+// main Component
+const Controller = ({ term, handleSearch, toggleForm }) => (
+  <div>
+    <SearchPanel
+      term={term}
+      handleSearch={handleSearch}
+      toggleForm={toggleForm}
+    />
+  </div>
+);
+
+// propTypes for Controller
+Controller.propTypes = {
+  term: PropTypes.string.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  toggleForm: PropTypes.func.isRequired,
+};
